5395fcc6c9c3ebcae51865e764225a711adbe9cc
chapaev <chapaevcs@gmail.com>
2021-02-16 23:55:54 +0200

    BFS.isValidCell более не нужен (совсем за него забыли)

--- src/main/java/com/airatgaliev/hourse_min_path/service/BFS.java
+++ src/main/java/com/airatgaliev/hourse_min_path/service/BFS.java
@@ -58,7 +58,4 @@ public class BFS<T extends Chessman> {
 
     }
 
-    public boolean isValidCell(int x, int y, int width, int height) {
-        return x >= 0 && x < width && y >= 0 && y < height;
-    }
 }
