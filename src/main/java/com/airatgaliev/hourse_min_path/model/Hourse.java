af3569c851eebd4cecbcd0ad7ff608c5e48125c0
chapaev <chapaevcs@gmail.com>
2021-02-16 23:51:34 +0200

    Убрал публичный доступ к внутренностям Hourse

--- src/main/java/com/airatgaliev/hourse_min_path/model/Hourse.java
+++ src/main/java/com/airatgaliev/hourse_min_path/model/Hourse.java
@@ -11,16 +11,6 @@ public class Hourse implements Chessman {
     private int dy[] = {-1, -2, -2, -1, 1, 2, 2, 1};
 
     @Override
-    public int getDx(int index) {
-        return dx[index];
-    }
-
-    @Override
-    public int getDy(int index) {
-        return dy[index];
-    }
-
-    @Override
     public ChessBoardCell getNextCellFrom(ChessBoardCell from, int possibleMovement) {
         int x1 = from.getX() + dx[possibleMovement];
         int y1 = from.getY() + dy[possibleMovement];
--- src/main/java/com/airatgaliev/hourse_min_path/model/interfaces/Chessman.java
+++ src/main/java/com/airatgaliev/hourse_min_path/model/interfaces/Chessman.java
@@ -4,10 +4,6 @@ import com.airatgaliev.hourse_min_path.model.ChessBoardCell;
 
 public interface Chessman {
 
-    int getDx(int index);
-
-    int getDy(int index);
-
     ChessBoardCell getNextCellFrom(ChessBoardCell from, int possibleMovement);
 
     int getPossibleMovementCnt();
