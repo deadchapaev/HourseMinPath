7535128c0a2759edd76bd41f3ccdf9137f2e9722
chapaev <chapaevcs@gmail.com>
2021-03-08 15:20:47 +0200

    Controller: здесь напрашивается отдельный метод, ибо цель trycatch отдать minPath - пофиксили

--- src/main/java/com/airatgaliev/hourse_min_path/controller/HoursePathController.java
+++ src/main/java/com/airatgaliev/hourse_min_path/controller/HoursePathController.java
@@ -33,7 +33,15 @@ public class HoursePathController {
             @RequestParam(value = "height", defaultValue = "8") int height,
             @RequestParam(value = "start", defaultValue = "A1") String start,
             @RequestParam(value = "end", defaultValue = "H8") String end) {
-        //TODO:здесь напрашивается отдельный метод, ибо цель trycatch отдать minPath
+        int minPath = getMinPath(start, end, width, height);
+        Count count = new Count(String.format(TEMPLATE, minPath));
+        count.add(linkTo(methodOn(HoursePathController.class).count(width, height, start.toUpperCase(), end.toUpperCase())).withSelfRel());
+
+        return new ResponseEntity<>(count, HttpStatus.OK);
+
+    }
+
+    private int getMinPath(String start, String end, int width, int height) throws NumberFormatException {
         int minPath;
         try {
             int startX = NumberFromExcelColumn(start.replaceAll("[0-9]", ""));
@@ -44,11 +52,7 @@ public class HoursePathController {
         } catch (NullPointerException e) {
             minPath = -1;
         }
-        Count count = new Count(String.format(TEMPLATE, minPath));
-        count.add(linkTo(methodOn(HoursePathController.class).count(width, height, start.toUpperCase(), end.toUpperCase())).withSelfRel());
-
-        return new ResponseEntity<>(count, HttpStatus.OK);
-
+        return minPath;
     }
 
     public int NumberFromExcelColumn(String column) {
